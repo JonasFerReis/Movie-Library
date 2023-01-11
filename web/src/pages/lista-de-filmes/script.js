9eb7dc19ccaaf4b1bdd61e858f12672e5efa530a
Jonas <jonasreis74@hotmail.com>
2023-01-11 19:16:57 -0300

    :rocket: Faz o campo de buscar funcionar

--- web/src/pages/lista-de-filmes/script.js
+++ web/src/pages/lista-de-filmes/script.js
@@ -1,32 +1,41 @@
 var listaDeFilmes = document.querySelector(".listaDeFilmes")
-var p1 = document.querySelector("#carregando")
+var dados = []
 
-axios.get("http://localhost:3333/filmes").then((response) => {
-    listaDeFilmes.removeChild(p1)
+function criarElemento(filme) {
+    var p = document.createElement("p")
+    p.innerHTML = filme.nome
+    p.addEventListener("click", () => {
+        location.href = `../dados-do-filme/index.html?id=${filme.id}`
+    })
+    listaDeFilmes.appendChild(p)
+}
 
-    if (response.data.length > 0) {
-        response.data.map((filme) => {
-            var p2 = document.createElement("p")
-            p2.innerHTML = filme.nome
-            p2.addEventListener("click", () => {
-                location.href = `../dados-do-filme/index.html?id=${filme.id}`
-            })
-            listaDeFilmes.appendChild(p2)
-        })
-    }
-    else {
-        var listaVazia = document.createElement("p")
-        listaVazia.innerHTML = "Nenhum filme cadastrado"
-        listaDeFilmes.appendChild(listaVazia)
-    }
+function buscar() {
+    var campoBuscar = document.querySelector(".campoBuscar").children[0].value
+    var filter = dados.filter((filme) => filme.nome.startsWith(campoBuscar))
 
-}).catch((err) => {
-    listaDeFilmes.removeChild(p1)
+    listaDeFilmes.innerHTML = ""
+    filter.map((filme) => criarElemento(filme))
+}
 
-    var erro = document.createElement("p")
-    erro.innerHTML = "ERRO: " + err
-    listaDeFilmes.appendChild(erro)
-})
+axios.get(`http://localhost:3333/filmes`)
+    .then((res) => {
+        listaDeFilmes.innerHTML = ""
 
+        if (res.data.length > 0) {
+            res.data.map((filme) => criarElemento(filme))
+            dados = res.data
+        }
+        else {
+            var listaVazia = document.createElement("span")
+            listaVazia.innerHTML = "Nenhum filme cadastrado"
+            listaDeFilmes.appendChild(listaVazia)
+        }
+    })
+    .catch((err) => {
+        listaDeFilmes.innerHTML = ""
 
-
+        var erro = document.createElement("span")
+        erro.innerHTML = "ERRO: " + err
+        listaDeFilmes.appendChild(erro)
+    })
